b9d31baef5b0ad537e5300f3bf06fe6e4a59f888
Ariana Habas <ariana.habas@slalom.com>
2022-01-27 13:20:36 -0600

    created basic express backend with basic routes

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json("API running...")
+})
+
+app.get('/product', (req, res) => {
+    res.json(products)
+})
+
+app.get('/product/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id)
+    res.json(product)
+})
+
+app.listen(5000, console.log('Server running on port 5000'))
