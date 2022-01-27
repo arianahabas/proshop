88023e2b05df6c2e16775e3e46d240b9b2541465
Ariana Habas <ariana.habas@slalom.com>
2022-01-27 16:23:01 -0600

    added env

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const products = require('./data/products')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express()
 
@@ -7,13 +10,15 @@ app.get('/', (req, res) => {
     res.json("API running...")
 })
 
-app.get('/product', (req, res) => {
+app.get('/api/products', (req, res) => {
     res.json(products)
 })
 
-app.get('/product/:id', (req, res) => {
+app.get('/api/products/:id', (req, res) => {
     const product = products.find(p => p._id === req.params.id)
     res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
